ccd040e6e6ac1e103386a5bad05a8fd2acc35c34
Jayvee <jayveetolibas@gmail.com>
2020-05-31 15:02:53 +0800

    added useState hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person";
 import ClassBasedComponent from "./ClassBasedComponent";
 import FunctionalComponent from "./FunctionalComponent";
 
 const App = (props) => {
+  const [personsState, setPersonState] = useState({
+    persons: [
+      {
+        name: "Jayvee",
+        age: 23,
+      },
+      {
+        name: "Jv",
+        age: 23,
+      },
+    ],
+  });
+
+  const [otherState, setOtherState] = useState({
+    otherState: "Some state",
+  });
+
+  console.log(personsState, otherState);
+
+  const nameHandler = () => {
+    // console.log("click");
+    // DONT DO THIS > this.state.persons[0].name = "JV";
+    setPersonState({
+      persons: [
+        {
+          name: "Jv",
+          age: 23,
+        },
+        {
+          name: "Jayvee",
+          age: 23,
+        },
+      ],
+    });
+
+    setOtherState({
+      otherState: "Some new state",
+    });
+  };
+
   return (
     <div className="App">
       <h1>Hi, Welcome to React Complete Guide.</h1>
-      <button onClick={this.nameHandler}>Switch name</button>
+      <button onClick={nameHandler}>Switch name</button>
       <Person
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age}
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
       />
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
         Hobbies: Gaming
       </Person>
       <ClassBasedComponent author="Jayvee" />
